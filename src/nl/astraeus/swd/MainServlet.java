d759985be875598374a69a465b62a37aba8dc065
Rien Nentjes <rien@nentjes.com>
2012-06-15 18:21:37 +0200

    Added Jetty, ext libs
    More REST
    Merge branch 'master' of github.com:rnentjes/Simple-forum-example
    
    Conflicts:
    	src/nl/astraeus/forum/web/ForumServlet.java

---
+++ src/nl/astraeus/swd/MainServlet.java
@@ -0,0 +1,33 @@
+package nl.astraeus.swd;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: rnentjes
+ * Date: 6/14/12
+ * Time: 7:53 PM
+ */
+public class MainServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // show page
+
+
+        // wait for input
+
+
+        // action depending on input
+        // if this link; open page
+        // if this link; open bla
+
+        // bla = new Page();
+        // page.show();
+        // done here
+    }
+}
---
+++ src/nl/astraeus/swd/Page.java
@@ -0,0 +1,16 @@
+package nl.astraeus.swd;
+
+/**
+ * User: rnentjes
+ * Date: 6/14/12
+ * Time: 8:03 PM
+ */
+public abstract class Page {
+
+    public abstract void show();
+
+    public void waitForInput() {
+
+    }
+
+}
